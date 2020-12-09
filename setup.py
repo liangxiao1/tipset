d0c1d9e9cc745ebe675437bdf9e1e339be7cb33b
Xiao Liang <xiliang@redhat.com>
2020-12-09 10:07:51 +0800

    v0.0.6
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setuptools.setup(
     name="tipset",
-    version="0.0.5",
+    version="0.0.6",
     author="Xiao Liang",
     author_email="xiliang@redhat.com",
     description="tipset is a colletion of mini tools about various tips under linux.",
