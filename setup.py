8739f05ee8ac1cd10933fc7fbc2b92aa1184a99d
Xiao Liang <xiliang@redhat.com>
2022-02-16 22:07:18 +0800

    bump to v0.0.15
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setuptools.setup(
     name="tipset",
-    version="0.0.14",
+    version="0.0.15",
     author="Xiao Liang",
     author_email="xiliang@redhat.com",
     description="tipset is a colletion of mini tools about various tips under linux.",
